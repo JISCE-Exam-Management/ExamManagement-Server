5a3b81d2b340300b75704fc5718d309e0f596285
Prasun Sarkar <prasunsarkar.personal@gmail.com>
2022-08-05 00:37:34 +0530

    added user routes

--- Models/userModel.js
+++ Models/userModel.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
 module.exports = mongoose.model('User', mongoose.Schema({
-    
+    name: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    isVerified: {type: Boolean, required: true, default: false}
 }));
--- Routers/userRoutes.js
+++ Routers/userRoutes.js
@@ -2,4 +2,59 @@ const express = require('express');
 const router = express.Router();
 const User = require('../Models/userModel');
 
+router.get('/all', async(req, res)=> {
+    try {
+        res.status(200).json((await User.find()).sort((a,b) => a.verified - b.verified));
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+router.get('/:id', async(req, res)=> {
+    try {
+        res.status(200).json(await User.findById(req.params.id));
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+router.post('/signup', async (req, res) => {
+    try {
+        const user = await User.findOne({email: req.body.email});
+        if (user) throw new Error("User already exists with this email!");
+        else res.status(200).json(await new User(req.body).save());
+    } catch (err) {
+        res.status(400).send(err);
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({email: req.body.email});
+        if (user) {
+            if (user.password === req.body.password) {
+                res.status(200).json(user);
+            } else throw new Error("Incorrect password!");
+        } else throw new Error("No user found!");
+    } catch (err) {
+        res.status(400).send(err);
+    }
+});
+
+router.patch('/update/:id', async(req, res)=> {
+    try {
+        res.status(200).json(await User.findByIdAndUpdate(req.params.id, req.body, {new: true}));
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+router.delete('/delete/:id', async(req, res)=> {
+    try {
+        res.status(200).json(await User.findByIdAndDelete(req.params.id));
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
 module.exports = router;
