cf6077ab47388a0b66168eaa5bc81c415c1018af
Prasun Sarkar <prasunsarkar.personal@gmail.com>
2022-08-07 13:12:57 +0530

    update

--- Extra Schemas/hallSchema.js
+++ Extra Schemas/hallSchema.js
@@ -1,4 +1,6 @@
 module.exports = require('mongoose').Schema({
     name: {type: String, required: true},
+    updatedBy: {type: String, required: false},
+    updatedTime: {type: Number, required: false},
     candidates: {type: Map, of: Boolean, required: false}
 });
