e3038f8b42417c30b24803ce341c503937227543
Prasun Sarkar <prasunsarkar.personal@gmail.com>
2022-08-10 15:28:18 +0530

    test

--- Routers/userRoutes.js
+++ Routers/userRoutes.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../Models/userModel');
-const Error = require('../Helpers/errors').default;
 
 router.get('/all', async(req, res)=> {
     try {
         res.status(200).json((await User.find(req.body, {password: 0})).sort((a,b) => a.verified - b.verified));
     } catch (error) {
-        res.status(400).send(error);
+        res.status(400).send(error.message);
     }
 });
 
@@ -15,7 +14,7 @@ router.get('/', async(req, res)=> {
     try {
         res.status(200).json(await User.findById(req.query.user, {password: 0}));
     } catch (error) {
-        res.status(400).send(error);
+        res.status(400).send(error.message);
     }
 });
 
@@ -25,7 +24,7 @@ router.post('/signup', async (req, res) => {
         if (user) throw new Error("User already exists with this email!");
         else res.status(200).json(await new User(req.body).save());
     } catch (err) {
-        res.status(400).send(err);
+        res.status(400).send(err.message);
     }
 });
 
@@ -33,12 +32,10 @@ router.post('/login', async (req, res) => {
     try {
         const user = await User.findOne({email: req.body.email});
         if (user) {
-            if (user.password === req.body.password) {
-                res.status(200).json(user);
-            } else throw new Error("Incorrect password!");
+            if (user.password === req.body.password) res.status(200).json(user); 
+            else throw new Error("Incorrect password!");
         } else throw new Error("No user found!");
     } catch (err) {
-        console.log(err);
         res.status(400).send(err.message);
     }
 });
@@ -47,7 +44,7 @@ router.patch('/update', async(req, res)=> {
     try {
         res.status(200).json(await User.findByIdAndUpdate(req.query.user, req.body, {new: true}));
     } catch (error) {
-        res.status(400).send(error);
+        res.status(400).send(error.message);
     }
 });
 
@@ -55,7 +52,7 @@ router.delete('/delete', async(req, res)=> {
     try {
         res.status(200).json(await User.findByIdAndDelete(req.query.user));
     } catch (error) {
-        res.status(400).send(error);
+        res.status(400).send(error.message);
     }
 });
 
