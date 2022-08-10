340aee33c5bf82e25c1590cd68cbe3d33761c468
Prasun Sarkar <prasunsarkar.personal@gmail.com>
2022-08-10 13:07:08 +0530

    test

---
+++ Helpers/errors.js
@@ -0,0 +1,13 @@
+class BaseError extends Error {
+    constructor(name, description="Bad Request", statusCode=400, isOperational=true) {
+        super(description);
+
+        Object.setPrototypeOf(this, new.target.prototype);
+        this.name = name;
+        this.statusCode = statusCode;
+        this.isOperational = isOperational;
+        Error.captureStackTrace(this);
+    }
+}
+
+module.exports = BaseError;
--- Routers/userRoutes.js
+++ Routers/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../Models/userModel');
+const BaseError = require('../Helpers/errors');
 
 router.get('/all', async(req, res)=> {
     try {
@@ -21,7 +22,7 @@ router.get('/', async(req, res)=> {
 router.post('/signup', async (req, res) => {
     try {
         const user = await User.findOne({email: req.body.email});
-        if (user) throw new Error("User already exists with this email!");
+        if (user) throw new BaseError("User already exists with this email!");
         else res.status(200).json(await new User(req.body).save());
     } catch (err) {
         res.status(400).send(err);
@@ -34,8 +35,8 @@ router.post('/login', async (req, res) => {
         if (user) {
             if (user.password === req.body.password) {
                 res.status(200).json(user);
-            } else throw new Error("Incorrect password!");
-        } else throw new Error("No user found!");
+            } else throw new BaseError("Incorrect password!");
+        } else throw new BaseError("No user found!");
     } catch (err) {
         res.status(400).send(err);
     }
